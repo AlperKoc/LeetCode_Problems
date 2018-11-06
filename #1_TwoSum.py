9af9fef5c16b01313e657d6974e5cc2d2b2e2b2a
AlperKoc <Alper.Koc@windowslive.com>
2018-11-06 16:04:30 +0300

    Update #1_TwoSum.py

--- #1_TwoSum.py
+++ #1_TwoSum.py
@@ -1,5 +1,17 @@
-def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums, target):
+        
+        numsOriginal = nums.copy()
+        nums.sort()
+        
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i,j]
+                    i1 = numsOriginal.index(nums[i])
+                    numsOriginal[i1] = numsOriginal[i1] + 1 # test case [3,3]
+                    i2 = numsOriginal.index(nums[j])
+                    return [i1,i2]
+                elif nums[i] + nums[j] > target:
+                    break
+            
+        
